915286a5374b4800d72152a937cf166322673c6a
passion <1434119806@qq.com>
2019-01-14 22:30:31 +0800

    权限管理系统常量定义代码修改提交。

---
+++ app/constant/constant.js
@@ -0,0 +1,8 @@
+const constant = {
+
+    enable_one: 1,
+    disable_zero: 0
+
+};
+
+module.exports = constant
--- app/constant/dbConstant.js
+++ app/constant/dbConstant.js
@@ -1,16 +1,16 @@
 const table = {
-    baseUser: "t_baseuser",
-    organization: "t_organization",
-    role: "t_role",
-    permission: "t_permission",
-    group: "t_group",
-    userGroup: "t_user_group",
-    userPermission: "t_user_permission",
-    userRole: "t_user_role",
-    groupRole: "t_group_role",
-    groupPermission: "t_group_permission",
-    rolePermission: "t_role_permission",
-    actionLog: "t_action_log",
+    baseUser: "t_baseuser",//系统用户相关
+    organization: "t_organization",//系统组织机构
+    role: "t_role",//用户角色
+    permission: "t_permission",//用户权限
+    group: "t_group",//分组
+    userGroup: "t_user_group", //用户和分组的关系
+    userPermission: "t_user_permission",//用户和权限关系
+    userRole: "t_user_role",//用户和角色关系
+    groupRole: "t_group_role",//分组和角色关系
+    groupPermission: "t_group_permission",//分组和权限关系
+    rolePermission: "t_role_permission",//角色和权限
+    actionLog: "t_action_log",//用户操作日志
 };
 
 module.exports = {table,};
