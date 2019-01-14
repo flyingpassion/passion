a4fffa5bf2d488e5a65385238f08c4227059e54f
passion <1434119806@qq.com>
2019-01-14 22:35:32 +0800

    权限管理系统核心路由分发代码修改提交。

--- app/router.js
+++ app/router.js
@@ -5,6 +5,12 @@
  */
 
 const userRouter = require("./router/userRouter");
+const organizationRouter = require("./router/organizationRouter");
+const roleRouter = require("./router/roleRouter");
+const permissionRouter = require("./router/permissionRouter");
+
+const groupRouter = require("./router/groupRouter");
+//const groupRouter = require("./router/groupRouter");
 
 
 module.exports = async (app) => {
@@ -12,6 +18,20 @@ module.exports = async (app) => {
 
     const {router, controller} = app;
 
+    //用户相关路由
     await userRouter.loadRouter(router, controller);
+    //组织相关
+    await organizationRouter.loadRouter(router, controller);
+
+    //角色相关
+    await roleRouter.loadRouter(router, controller);
+
+    //用户权限
+    await permissionRouter.loadRouter(router, controller);
+
+    //
+    await groupRouter.loadRouter(router, controller);
+    // await groupRouter.loadRouter(router, controller);
+
 
 };
